c21f0bdfe156c012de10e11107760e8341201aed
mak2220 <113451174+mak2220@users.noreply.github.com>
2023-01-20 12:30:49 -0300

    Add files via upload

---
+++ fecha.js
@@ -0,0 +1,12 @@
+const hoy = new Date();
+
+const miNacimineto = new Date(1983,08,19);
+
+const variable = hoy.getTime() > miNacimineto.getTime();
+
+const diaNacimineto = miNacimineto.getDate();
+
+const mesNacimineto = miNacimineto.getMonth()+1;
+
+const añoNacimineto = miNacimineto.getFullYear();
+
